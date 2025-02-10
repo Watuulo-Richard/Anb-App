2ec62f3ea29574880764de19acc20cca482e88eb
Watuulo-Richard <watuulorichard@gmail.com>
2025-02-10 18:36:26 +0300

    removed unecessary code

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -2,16 +2,13 @@
 
 import { fetchCategories } from "@/action/fetch";
 import { CategoryNav } from "@/components/categorysection";
-// import FrontViewCardSection from "@/components/frontviewcardsection";
 
 export default async function page() {
   const fetchedCategories = await fetchCategories()
-  // const fetchedProducts = await fetchProducts()
   return (
     <>
       {/* <NavBar /> */}
       <CategoryNav receivedCategoriesFromFetch = {fetchedCategories}/>
-      {/* <FrontViewCardSection receivedProductsFromFetch = {fetchedProducts}/> */}
     </>
   );
 }
